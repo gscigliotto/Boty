dd5e4a154e9a56b825c4fe5e35b54cc84529bb8e
gscigliotto <gscigliotto@gmail.com>
2020-10-28 00:07:36 -0300

    cambio al puerto 80

--- View/app.py
+++ View/app.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     import os
     HOST = os.environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+        PORT = int(os.environ.get('SERVER_PORT', '80'))
     except ValueError:
-        PORT = 5555
+        PORT = 80
     app.run(HOST, PORT)
